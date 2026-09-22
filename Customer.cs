01cbaccda2b68d63d9da746bca0c5def1e1e6852
Michael Rusnok <mrusnokk@eduroam-169-146.vsb.cz>
2026-09-22 09:40:17 +0200

    zakladni kostra

---
+++ Customer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
